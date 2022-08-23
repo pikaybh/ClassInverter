0bd3275971f9e46ad997d6fb0e5bcee972e23a6c
pikaybh <pikabh@naver.com>
2022-08-24 00:02:08 +0900

    bug 수정

--- classInverter.py
+++ classInverter.py
@@ -5,13 +5,14 @@ foo = input('[INFO] Index of class: ')
 os.chdir("./")
 
 for txt in glob.glob("*.txt"):
-    f = open(txt, 'r')
-    line = f.read()
-    letters = line.split()
-    letters[0]=str(foo)
-    letters = " ".join(letters)
-    f = open(txt, 'w')
-    f.write(letters)
-    f.close()
+    if os.path.basename(txt) != "classes.txt":
+        f = open(txt, 'r')
+        line = f.read()
+        letters = line.split()
+        letters[0]=str(foo)
+        letters = " ".join(letters)
+        f = open(txt, 'w')
+        f.write(letters)
+        f.close()
 
 print('[INFO] Done!')
